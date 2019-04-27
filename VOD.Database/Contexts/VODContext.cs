507a305a59aa31938cc820f26ee475a4711bbdfe
Jonas <N/A>
2019-04-27 13:11:39 +0200

    Adding the Database Context

---
+++ VOD.Database/Contexts/VODContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VOD.Common.Entities;
+
+namespace VOD.Database.Contexts
+{
+    public class VODContext : IdentityDbContext<VODUser>
+    {
+        #region Constructor
+        public VODContext(DbContextOptions<VODContext> options) : base(options)
+        {
+        }
+        #endregion
+
+        #region Overrides
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        #endregion
+    }
+}
