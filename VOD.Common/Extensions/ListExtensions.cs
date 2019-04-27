f78191d934ca020ba433ac9edd5da004baa371ae
Jonas <N/A>
2019-04-27 18:09:19 +0200

    Converting an Entity Collection to a List of SelectList Items (ToSelectList)

---
+++ VOD.Common/Extensions/ListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace VOD.Common.Extensions
+{
+    public static class ListExtensions
+    {
+        public static SelectList ToSelectList<TEntity>(this List<TEntity> items, string valueField, string textField) where TEntity : class
+        {
+            return new SelectList(items, valueField, textField);
+        }
+
+    }
+}
