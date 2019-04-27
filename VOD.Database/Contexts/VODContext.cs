a7b41396b1f222c4ee525694f5a9f38354a02f3f
Jonas <N/A>
2019-04-27 16:01:56 +0200

    Adding the Tables

--- VOD.Database/Contexts/VODContext.cs
+++ VOD.Database/Contexts/VODContext.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using VOD.Common.Entities;
 
 namespace VOD.Database.Contexts
 {
     public class VODContext : IdentityDbContext<VODUser>
     {
+        #region DbSet Properties
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Download> Downloads { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<UserCourse> UserCourses { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        #endregion
+
         #region Constructor
         public VODContext(DbContextOptions<VODContext> options) : base(options)
         {
@@ -16,6 +26,15 @@ namespace VOD.Database.Contexts
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            // Composite key
+            builder.Entity<UserCourse>().HasKey(uc => new { uc.UserId, uc.CourseId });
+
+            // Restrict cascading deletes
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
         }
         #endregion
     }
