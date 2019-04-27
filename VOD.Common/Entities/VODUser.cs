9e3c197c74b094e68da7de595d810e04d72cb194
Jonas <N/A>
2019-04-27 13:07:35 +0200

    Adding the VODUser Class

---
+++ VOD.Common/Entities/VODUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Claims;
+using System.Text;
+
+namespace VOD.Common.Entities
+{
+    public class VODUser : IdentityUser
+    {
+        public string Token { get; set; }
+        public DateTime TokenExpires { get; set; }
+
+        [NotMapped]
+        public IList<Claim> Claims { get; set; } = new List<Claim>();
+    }
+}
