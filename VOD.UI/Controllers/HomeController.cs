4d463b675fa787a1fab53a76a20a462adf175da6
Jonas <N/A>
2019-04-27 18:36:16 +0200

    Removed the test code from the HomeController

--- VOD.UI/Controllers/HomeController.cs
+++ VOD.UI/Controllers/HomeController.cs
@@ -15,27 +15,18 @@ namespace VOD.UI.Controllers
     {
         #region Properties
         private SignInManager<VODUser> _signInManager;
-        private IDbReadService _db;
         #endregion
 
         #region Constructor
-        public HomeController(SignInManager<VODUser> signInMgr, IDbReadService db)
+        public HomeController(SignInManager<VODUser> signInMgr)
         {
             _signInManager = signInMgr;
-            _db = db;
         }
         #endregion
 
         #region Actions
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            _db.Include<Download>();
-            _db.Include<Module, Course>();
-            var result1 = await _db.SingleAsync<Download>(d => d.Id.Equals(3));
-            var result2 = await _db.GetAsync<Download>(); // Fetch all
-            var result3 = await _db.GetAsync<Download>(d => d.ModuleId.Equals(1)); // Fetch all that matches the Lambda expression
-            var result4 = await _db.AnyAsync<Download>(d => d.ModuleId.Equals(1)); // True if a record is found
-            
             if (!_signInManager.IsSignedIn(User))
                 return RedirectToPage("/Account/Login", new { Area = "Identity" });
 
