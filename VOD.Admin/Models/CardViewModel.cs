e712a7cb8b639c7c3d2dbf49b51fd25a1e0bcc2f
Jonas <N/A>
2019-04-28 18:35:30 +0200

    Adding the CardViewModel Class

---
+++ VOD.Admin/Models/CardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace VOD.Admin.Models
+{
+    public class CardViewModel
+    {
+        public int Count { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string Url { get; set; }
+        public string BackgroundColor { get; set; }
+    }
+}
