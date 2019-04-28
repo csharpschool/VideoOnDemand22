04b164080e6311af5c00f24647dc9747d8a63ba1
Jonas <N/A>
2019-04-28 07:46:33 +0200

    Adding the IUIReadService Interface

---
+++ VOD.Database/Services/IUIReadService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VOD.Common.Entities;
+
+namespace VOD.Database.Services
+{
+    public interface IUIReadService
+    {
+        Task<IEnumerable<Course>> GetCourses(string userId);
+        Task<Course> GetCourse(string userId, int courseId);
+        Task<Video> GetVideo(string userId, int videoId);
+        Task<IEnumerable<Video>> GetVideos(string userId, int moduleId = default(int));
+    }
+}
