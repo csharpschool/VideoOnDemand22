8d32918849a3dc84989e7adec2e3361669b25c58
Jonas <N/A>
2019-04-28 09:10:08 +0200

    Adding the Membership Controller

---
+++ VOD.UI/Controllers/MembershipController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VOD.Common.Entities;
+using VOD.Database.Services;
+
+namespace VOD.UI.Controllers
+{
+    public class MembershipController : Controller
+    {
+        #region Properties
+        private readonly string _userId;
+        private readonly IMapper _mapper;
+        private readonly IUIReadService _db;
+        #endregion
+
+        #region Constructor
+        public MembershipController(IHttpContextAccessor httpContextAccessor, UserManager<VODUser> userManager, IMapper mapper, IUIReadService db)
+        {
+            var user = httpContextAccessor.HttpContext.User;
+            _userId = userManager.GetUserId(user);
+            _mapper = mapper;
+            _db = db;
+        }
+        #endregion
+
+        #region Action Methods
+        [HttpGet]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Course(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Video(int id)
+        {
+            return View();
+        }
+        #endregion
+
+    }
+}
