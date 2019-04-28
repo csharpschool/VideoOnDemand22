0db2997cd62ecaf4471e8064db24c2db0f49a824
Jonas <N/A>
2019-04-28 09:55:28 +0200

    The Dashboard Action Method

--- VOD.UI/Controllers/MembershipController.cs
+++ VOD.UI/Controllers/MembershipController.cs
@@ -6,8 +6,10 @@ using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using VOD.Common.DTOModels.UI;
 using VOD.Common.Entities;
 using VOD.Database.Services;
+using VOD.UI.Models.MembershipViewModels;
 
 namespace VOD.UI.Controllers
 {
@@ -31,19 +33,32 @@ namespace VOD.UI.Controllers
 
         #region Action Methods
         [HttpGet]
-        public IActionResult Dashboard()
+        public async Task<IActionResult> Dashboard()
+        {
+            var courseDtoObjects = _mapper.Map<List<CourseDTO>>(await _db.GetCourses(_userId));
+            var dashboardModel = new DashboardViewModel();
+            dashboardModel.Courses = new List<List<CourseDTO>>();
+
+            var noOfRows = courseDtoObjects.Count <= 3 ? 1 : courseDtoObjects.Count / 3;
+            for (var i = 0; i < noOfRows; i++)
+            {
+                dashboardModel.Courses.Add(courseDtoObjects.Skip(i * 3).Take(3).ToList());
+            }
+
+            return View(dashboardModel);
+        }
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Course(int id)
         {
             return View();
         }
 
         [HttpGet]
-        public IActionResult Course(int id)
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Video(int id)
+        public async Task<IActionResult> Video(int id)
         {
             return View();
         }
--- VOD.UI/Startup.cs
+++ VOD.UI/Startup.cs
@@ -51,34 +51,23 @@ namespace VOD.UI
                 cfg.CreateMap<Video, VideoDTO>();
 
                 cfg.CreateMap<Instructor, InstructorDTO>()
-                    .ForMember(dest => dest.InstructorName,
-                        src => src.MapFrom(s => s.Name))
-                    .ForMember(dest => dest.InstructorDescription,
-                        src => src.MapFrom(s => s.Description))
-                    .ForMember(dest => dest.InstructorAvatar,
-                        src => src.MapFrom(s => s.Thumbnail));
+                    .ForMember(dest => dest.InstructorName, src => src.MapFrom(s => s.Name))
+                    .ForMember(dest => dest.InstructorDescription, src => src.MapFrom(s => s.Description))
+                    .ForMember(dest => dest.InstructorAvatar, src => src.MapFrom(s => s.Thumbnail));
 
                 cfg.CreateMap<Download, DownloadDTO>()
-                    .ForMember(dest => dest.DownloadUrl,
-                        src => src.MapFrom(s => s.Url))
-                    .ForMember(dest => dest.DownloadTitle,
-                        src => src.MapFrom(s => s.Title));
+                    .ForMember(dest => dest.DownloadUrl, src => src.MapFrom(s => s.Url))
+                    .ForMember(dest => dest.DownloadTitle, src => src.MapFrom(s => s.Title));
 
                 cfg.CreateMap<Course, CourseDTO>()
-                    .ForMember(dest => dest.CourseId, src =>
-                        src.MapFrom(s => s.Id))
-                    .ForMember(dest => dest.CourseTitle,
-                        src => src.MapFrom(s => s.Title))
-                    .ForMember(dest => dest.CourseDescription,
-                        src => src.MapFrom(s => s.Description))
-                    .ForMember(dest => dest.MarqueeImageUrl,
-                        src => src.MapFrom(s => s.MarqueeImageUrl))
-                    .ForMember(dest => dest.CourseImageUrl,
-                        src => src.MapFrom(s => s.ImageUrl));
+                    .ForMember(dest => dest.CourseId, src => src.MapFrom(s => s.Id))
+                    .ForMember(dest => dest.CourseTitle, src => src.MapFrom(s => s.Title)) 
+                    .ForMember(dest => dest.CourseDescription, src => src.MapFrom(s => s.Description))
+                    .ForMember(dest => dest.MarqueeImageUrl, src => src.MapFrom(s => s.MarqueeImageUrl))
+                    .ForMember(dest => dest.CourseImageUrl, src => src.MapFrom(s => s.ImageUrl));
 
                 cfg.CreateMap<Module, ModuleDTO>()
-                    .ForMember(dest => dest.ModuleTitle,
-                        src => src.MapFrom(s => s.Title));
+                    .ForMember(dest => dest.ModuleTitle, src => src.MapFrom(s => s.Title));
             });
             var mapper = config.CreateMapper();
             services.AddSingleton(mapper);
