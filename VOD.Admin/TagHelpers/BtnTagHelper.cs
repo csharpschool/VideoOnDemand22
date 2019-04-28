bdd7df0cf2e895d3ffd8a44d5f5f860927337c00
Jonas <N/A>
2019-04-28 19:37:59 +0200

    Creating the Tag Helper

---
+++ VOD.Admin/TagHelpers/BtnTagHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace VOD.Admin.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("btn")]
+    public class BtnTagHelper : AnchorTagHelper
+    {
+        #region Properties
+        public string Icon { get; set; } = string.Empty;
+        #endregion
+
+        #region Constants
+        const string btnPrimary = "btn-primary";
+        const string btnDanger = "btn-danger";
+        const string btnDefault = "btn-default";
+        const string btnInfo = "btn-info";
+        const string btnSucess = "btn-success";
+        const string btnWarning = "btn-warning";
+        // Google's Material Icons provider name
+        const string iconProvider = "material-icons";
+        #endregion
+
+        #region This constructor is needed for AnchorTagHelper inheritance
+        public BtnTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+        #endregion
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            // Changes <btn> tag to <a> tag when rendered
+            output.TagName = "a";
+
+            base.Process(context, output);
+        }
+    }
+}
