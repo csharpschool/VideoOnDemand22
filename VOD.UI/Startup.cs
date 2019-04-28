3963e13c9a6833665fc236a85f28af696c1230db
Jonas <N/A>
2019-04-28 10:59:28 +0200

    Adding the Dashboard View

--- VOD.UI/Startup.cs
+++ VOD.UI/Startup.cs
@@ -27,12 +27,12 @@ namespace VOD.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
 
             services.AddDbContext<VODContext>(options =>
                 options.UseSqlServer(
@@ -90,7 +90,7 @@ namespace VOD.UI
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            //app.UseCookiePolicy();
 
             // Uncomment to recreagte the database. ALL DATA WILL BE LOST !
             //DbInitializer.RecreateDatabase(db);
