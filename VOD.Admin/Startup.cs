1d41b8579c6ba2e5fa4dc0e9b70e279c8a4e4878
Jonas <N/A>
2019-04-29 11:58:33 +0200

    The Users/Index Razor Page

--- VOD.Admin/Startup.cs
+++ VOD.Admin/Startup.cs
@@ -49,6 +49,7 @@ namespace VOD.Admin
 
             services.AddScoped<IDbReadService, DbReadService>();
             services.AddScoped<IDbWriteService, DbWriteService>();
+            services.AddScoped<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
