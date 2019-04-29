14733f72f1067c954962435afb407756d1aad30e
Jonas <N/A>
2019-04-29 09:21:13 +0200

    Adding the ButtonDTO Class

---
+++ VOD.Common/DTOModels/Admin/ButtonDTO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VOD.Common.DTOModels.Admin
+{
+    public class ButtonDTO
+    {
+        #region Properties
+        public int CourseId { get; set; }
+        public int ModuleId { get; set; }
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string ItemId
+        {
+            get { return Id > 0 ? Id.ToString() : UserId; }
+        }
+        #endregion
+
+        #region Constructors
+        public ButtonDTO(int courseId, int moduleId, int id)
+        {
+            CourseId = courseId;
+            ModuleId = moduleId;
+            Id = id;
+        }
+
+        public ButtonDTO(int courseId, int id)
+        {
+            CourseId = courseId;
+            Id = id;
+        }
+
+        public ButtonDTO(int id)
+        {
+            Id = id;
+        }
+
+        public ButtonDTO(string userId)
+        {
+            UserId = userId;
+        }
+        #endregion
+    }
+}
