dcf0e792508f110b56605d408870f1ef3e1ec9c5
Jonas <N/A>
2019-04-29 09:50:16 +0200

    The LoginUserDTO Class

---
+++ VOD.Common/DTOModels/LoginUserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace VOD.Common.DTOModels
+{
+    public class LoginUserDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
