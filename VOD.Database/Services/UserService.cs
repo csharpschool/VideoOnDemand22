f1b8f4d76365c1660c9da08417e4332a57c122fc
Jonas <N/A>
2019-04-29 09:28:35 +0200

    Adding the Service Interface and Class

---
+++ VOD.Database/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using VOD.Common.Entities;
+using VOD.Database.Contexts;
+
+namespace VOD.Database.Services
+{
+    public class UserService : IUserService
+    {
+        #region Properties
+        private readonly VODContext _db;
+        private readonly UserManager<VODUser> _userManager;
+        #endregion
+
+        #region Constructor
+        public UserService(VODContext db, UserManager<VODUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+        #endregion
+    }
+}
