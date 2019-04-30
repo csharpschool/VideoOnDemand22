a606b4adab53ba023186800efc201c46fc88c183
Jonas <N/A>
2019-04-30 14:21:04 +0200

    Adding the Get Action Method

--- VOD.API/Controllers/InstructorsController.cs
+++ VOD.API/Controllers/InstructorsController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VOD.Common.DTOModels.Admin;
+using VOD.Common.Entities;
 using VOD.Common.Services;
 
 namespace VOD.API.Controllers
@@ -26,5 +29,20 @@ namespace VOD.API.Controllers
         }
         #endregion
 
+        #region Actions
+        [HttpGet()]
+        public async Task<ActionResult<List<InstructorDTO>>> Get(bool include = false)
+        {
+            try
+            {
+                return await _db.GetAsync<Instructor, InstructorDTO>(include);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        #endregion
     }
 }
