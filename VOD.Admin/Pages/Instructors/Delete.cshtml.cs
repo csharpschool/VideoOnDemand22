bdeab8d21a6993f9e0a1b943f2e7df321716591b
Jonas <N/A>
2019-04-30 08:54:09 +0200

    The DeleteModel Class

--- VOD.Admin/Pages/Instructors/Delete.cshtml.cs
+++ VOD.Admin/Pages/Instructors/Delete.cshtml.cs
@@ -2,46 +2,58 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using VOD.Common.DTOModels;
-using VOD.Database.Services;
+using VOD.Common.DTOModels.Admin;
+using VOD.Common.Entities;
+using VOD.Common.Services;
 
-namespace VOD.Admin.Pages.Users
+namespace VOD.Admin.Pages.Instructors
 {
     [Authorize(Roles = "Admin")]
     public class DeleteModel : PageModel
     {
         #region Properties
-        private readonly IUserService _userService;
-        [BindProperty] public UserDTO Input { get; set; } = new UserDTO();
+        private readonly IAdminService _db;
+        [BindProperty] public InstructorDTO Input { get; set; } = new InstructorDTO();
         [TempData] public string Alert { get; set; }
         #endregion
 
         #region Constructor
-        public DeleteModel(IUserService userService)
+        public DeleteModel(IAdminService db)
         {
-            _userService = userService;
+            _db = db;
         }
         #endregion
 
         #region Actions
-        public async Task OnGetAsync(string id)
+        public async Task<IActionResult> OnGetAsync(string id)
         {
-            Alert = string.Empty;
-            Input = await _userService.GetUserAsync(id);
+            try
+            {
+                Alert = string.Empty;
+                Input = await _db.SingleAsync<Instructor, InstructorDTO>(s => s.Id.Equals(id));
+                return Page();
+            }
+            catch
+            {
+                return RedirectToPage("/Index", new { alert = "You do not have access to this page." });
+            }
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
+            var id = Input.Id;
+
             if (ModelState.IsValid)
             {
-                var result = await _userService.DeleteUserAsync(Input.Id);
-                if (result)
+                var succeeded = await _db.DeleteAsync<Instructor>(d => d.Id.Equals(id));
+                if (succeeded)
                 {
-                    Alert = $"User {Input.Email} was deleted.";
+                    Alert = $"Deleted Instructor: {Input.Name}.";
                     return RedirectToPage("Index");
                 }
             }
 
+            Input = await _db.SingleAsync<Instructor, InstructorDTO>(s => s.Id.Equals(id));
             return Page();
         }
         #endregion
