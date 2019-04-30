5e0c83b76cb09dc2c7c88cb2571164de0e163958
Jonas <N/A>
2019-04-30 14:09:53 +0200

    Adding the API Project

---
+++ VOD.API/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using VOD.Common.Entities;
+using VOD.Common.Services;
+using VOD.Database.Contexts;
+using VOD.Database.Services;
+
+namespace VOD.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<VODContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<VODUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<VODContext>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //services.AddAutoMapper();                 // Version 6.0.0: AutoMapper.Extensions.Microsoft.DependencyInjection
+            services.AddAutoMapper(typeof(Startup));    // Version 6.1.0: AutoMapper.Extensions.Microsoft.DependencyInjection
+
+            services.AddScoped<IDbReadService, DbReadService>();
+            services.AddScoped<IDbWriteService, DbWriteService>();
+            services.AddScoped<IAdminService, AdminEFService>();
+            services.AddScoped<IUserService, UserService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
