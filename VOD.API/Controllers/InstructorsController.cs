222550c1db43e8713bf2a04170febb568bfe2bea
Jonas <N/A>
2019-04-30 14:13:00 +0200

    Adding the Instructors Controller

---
+++ VOD.API/Controllers/InstructorsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VOD.Common.Services;
+
+namespace VOD.API.Controllers
+{
+    [Route("api/instructors")]
+    [ApiController]
+    public class InstructorsController : ControllerBase
+    {
+        #region Properties and Variables
+        private readonly IAdminService _db;
+        private readonly LinkGenerator _linkGenerator;
+        #endregion
+
+        #region Constructor
+        public InstructorsController(IAdminService db, LinkGenerator
+        linkGenerator)
+        {
+            _db = db;
+            _linkGenerator = linkGenerator;
+        }
+        #endregion
+
+    }
+}
