154a0f227e966e03c827f30e1f58bd2569c8d7b0
Jonas <N/A>
2019-05-01 10:19:00 +0200

    Adding the HttpResponseException class

---
+++ VOD.Common/Exceptions/HttpResponseException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+
+namespace VOD.Common.Exceptions
+{
+    public class HttpResponseException : Exception
+    {
+        #region Properties
+        public HttpStatusCode HttpStatusCode { get; }
+        public object ValidationErrors { get; }
+        #endregion
+
+        #region Constructors
+        public HttpResponseException(HttpStatusCode status, string message, object validationErrors) : base(message)
+        {
+            HttpStatusCode = status;
+            ValidationErrors = validationErrors;
+        }
+        public HttpResponseException(HttpStatusCode status, string message) : this(status, message, null)
+        {
+            HttpStatusCode = status;
+        }
+        public HttpResponseException(HttpStatusCode status) : this(status, string.Empty, null)
+        {
+            HttpStatusCode = status;
+        }
+        #endregion
+    }
+}
