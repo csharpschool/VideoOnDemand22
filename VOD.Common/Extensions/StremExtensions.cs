fe71bac2854d9c156ae58cfcba79680fd1568f86
Jonas <N/A>
2019-05-01 10:34:48 +0200

    Deserialize from JSON to an Object of Generic Type

---
+++ VOD.Common/Extensions/StremExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VOD.Common.Extensions
+{
+    public static class StremExtensions
+    {
+        public static async Task SerializeToJsonAndWriteAsync<T>(this Stream stream, T objectToWrite, Encoding encoding, int bufferSize, bool leaveOpen, bool resetStream = false)
+        {
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+            if (!stream.CanWrite) throw new NotSupportedException("Can't write to this stream.");
+            if (encoding == null) throw new ArgumentNullException(nameof(encoding));
+
+            using (var streamWriter = new StreamWriter(stream, encoding, bufferSize, leaveOpen))
+            {
+                using (var jsonTextWriter = new JsonTextWriter(streamWriter))
+                {
+                    var jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(jsonTextWriter, objectToWrite);
+                    await jsonTextWriter.FlushAsync();
+                }
+            }
+
+            // after writing, set the stream to position 0
+            if (resetStream && stream.CanSeek) stream.Seek(0, SeekOrigin.Begin);
+        }
+        public static T ReadAndDeserializeFromJson<T>(this Stream stream)
+        {
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+            if (!stream.CanRead) throw new NotSupportedException("Can't read from this stream.");
+
+            using (var streamReader = new StreamReader(stream, new UTF8Encoding(), true, 1024, false))
+            {
+                using (var jsonTextReader = new JsonTextReader(streamReader))
+                {
+                    var jsonSerializer = new JsonSerializer();
+                    return jsonSerializer.Deserialize<T>(jsonTextReader);
+                }
+            }
+
+        }
+    }
+}
