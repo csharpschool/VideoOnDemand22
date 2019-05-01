94670787c59dd0dc599e369a3823a3a59b1e8720
Jonas <N/A>
2019-05-01 11:04:20 +0200

    Create an Extension Method That Throws an Exception Based on the Response Status Code

--- VOD.Common/Extensions/HttpClientExtensions.cs
+++ VOD.Common/Extensions/HttpClientExtensions.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using VOD.Common.Exceptions;
 
 namespace VOD.Common.Extensions
 {
@@ -36,6 +38,32 @@ namespace VOD.Common.Extensions
             response.EnsureSuccessStatusCode();
             var responseStream = await response.Content.ReadAsStreamAsync();
             return responseStream.ReadAndDeserializeFromJson<TResponse>();
+        }
+        private static async Task CheckStatusCodes(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                object validationErrors = null;
+                var message = string.Empty;
+                if (response.StatusCode == HttpStatusCode.UnprocessableEntity)
+                {
+                    var errorStream = await response.Content.ReadAsStreamAsync();
+                    validationErrors = errorStream.ReadAndDeserializeFromJson();
+                    message = "Could not process the entity.";
+                }
+                else if (response.StatusCode == HttpStatusCode.BadRequest)
+                    message = "Bad request.";
+                else if (response.StatusCode == HttpStatusCode.Forbidden)
+                    message = "Access denied.";
+                else if (response.StatusCode == HttpStatusCode.NotFound)
+                    message = "Could not find the entity.";
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                    message = "Not Logged In.";
+
+                throw new HttpResponseException(response.StatusCode, message,
+                    validationErrors);
+            }
+            else response.EnsureSuccessStatusCode();
         }
 
     }
