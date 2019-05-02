c5e4bafcf87f4f182c86ab7489c36718607332fe
Jonas <N/A>
2019-05-02 12:16:16 +0200

    JWT: Implementing the GetTokenAsync Method

--- VOD.Common/Services/JwtTokenService.cs
+++ VOD.Common/Services/JwtTokenService.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using System;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using VOD.Common.DTOModels;
 using VOD.Common.Entities;
+using VOD.Common.Extensions;
 
 namespace VOD.Common.Services
 {
@@ -49,7 +51,33 @@ namespace VOD.Common.Services
         }
         public async Task<TokenDTO> GetTokenAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                var user = await _userManager.FindByIdAsync(userId);
+                var claims = await _userManager.GetClaimsAsync(user);
+                var token = claims.Single(c => c.Type.Equals("Token")).Value;
+                var date = claims.Single(c => c.Type.Equals("TokenExpires")).Value;
+                DateTime expires;
+                var succeeded = DateTime.TryParse(date, out expires);
+
+                // Return token from the user object
+                if (succeeded && !token.IsNullOrEmptyOrWhiteSpace()) return new TokenDTO(token, expires);
+
+                // Return token from the API
+                var tokenUser = new LoginUserDTO
+                {
+                    Email = user.Email,
+                    Password = "",
+                    PasswordHash = user.PasswordHash
+                };
+                var newToken = await _http.GetTokenAsync(tokenUser, $"api/token/{user.Id}", "AdminClient");
+                return newToken;
+            }
+            catch
+            {
+                return default;
+            }
         }
         public async Task<TokenDTO> CheckTokenAsync(TokenDTO token)
         {
