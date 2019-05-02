db6cb9ffd6a16af9f3c4c69b4c004a5dedee25e2
Jonas <N/A>
2019-05-02 08:24:40 +0200

    JWT: Fething the User’s Claims

--- VOD.API/Services/TokenService.cs
+++ VOD.API/Services/TokenService.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using VOD.Common.DTOModels;
+using VOD.Common.Entities;
 using VOD.Database.Services;
 
 namespace VOD.API.Services
@@ -23,8 +26,27 @@ namespace VOD.API.Services
         }
         #endregion
 
-        #region Token Methods
+        #region Helper Methods
+        private List<Claim> GetClaims(VODUser user, bool includeUserClaims)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
 
+            if (includeUserClaims)
+                foreach (var claim in user.Claims)
+                    if (!claim.Type.Equals("Token") &&
+                        !claim.Type.Equals("TokenExpires")) claims.Add(claim);
+
+            return claims;
+        }
+
+        #endregion
+
+        #region Token Methods
         public Task<TokenDTO> GenerateTokenAsync(LoginUserDTO loginUserDto)
         {
             throw new NotImplementedException();
