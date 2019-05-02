45cae9b35fa01a3fa7229dd04e3c586eac366656
Jonas <N/A>
2019-05-02 12:05:01 +0200

    JWT: Adding the IJwtTokenService Interface

---
+++ VOD.Common/Services/IJwtTokenService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using VOD.Common.DTOModels;
+
+namespace VOD.Common.Services
+{
+    public interface IJwtTokenService
+    {
+        Task<TokenDTO> CreateTokenAsync();
+        Task<TokenDTO> GetTokenAsync();
+        Task<TokenDTO> CheckTokenAsync(TokenDTO token);
+    }
+}
