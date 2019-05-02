98837963dec32ba3c548a21197c76bb2a3492a9d
Jonas <N/A>
2019-05-02 08:07:03 +0200

    JWT: Adding the TokenDTO Class

---
+++ VOD.Common/DTOModels/TokenDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VOD.Common.DTOModels
+{
+    public class TokenDTO
+    {
+        public string Token { get; set; } = "";
+        public DateTime TokenExpires { get; set; } = default;
+        public bool TokenHasExpired
+        {
+            get
+            {
+                return TokenExpires == default ? true : !(TokenExpires.Subtract(DateTime.UtcNow).Minutes > 0);
+            }
+        }
+
+        public TokenDTO(string token, DateTime expires)
+        {
+            Token = token;
+            TokenExpires = expires;
+        }
+
+        public TokenDTO()
+        {
+        }
+    }
+}
